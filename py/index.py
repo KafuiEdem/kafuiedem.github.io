c8d3fe44a5ab8bc0dc0cfe8b0b917de4b70b70e2
Edem Robin <robinedemamedzo@gmail.com>
2023-07-15 13:35:07 +0000

    updating my index.py

--- py/index.py
+++ py/index.py
@@ -1,3 +1,19 @@
-import datetime
+from datetime import datetime
 
-# print("Hellow world")
+
+notice_date = Element("clock")
+
+current = datetime.now()
+
+year = current.year
+day = current.day
+month = current.month
+week_day = current.weekday()
+date = ""
+d = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+if week_day > 4:
+    date = f"WEEKEND \n{d[week_day]} {day} / {month} / {year}"
+else:
+    date += f"WEEKDAY \n{d[week_day]} {day} / {month} / {year}"
+
+notice_date.write(date)
